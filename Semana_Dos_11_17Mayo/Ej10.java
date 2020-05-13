6bcb7968021d816da1c4ab9a4f9f16db035713de
rociornav <alum.rdelr@iesalixar.org>
2020-05-13 10:07:43 +0200

    Segunda semana de refuerzo de la Evaluación 1 de Programación

--- Semana_Dos_11_17Mayo/Ej10.java
+++ Semana_Dos_11_17Mayo/Ej10.java
@@ -1,7 +1,25 @@
 package SemanaDos_11a17Mayo;
 
 import java.util.Scanner;
+/*
+Enunciado: 09/05/20 RRN
 
+Realiza un programa que pinte por pantalla una pecera con un pececito
+dentro.
+Se debe pedir al usuario el ancho y el alto de la pecera, que como
+mínimo serán de 4 unidades. No hay que comprobar que los datos se
+introducen correctamente; podemos suponer que el usuario los introduce
+bien. Dentro de la pecera hay que colocar de forma aleatoria un
+pececito, que puede estar situado en cualquiera de las posiciones que
+quedan en el hueco que forma el rectángulo.
+Ejemplo:
+Por favor, introduzca la altura de la pecera (como mínimo 4): 4
+Ahora introduzca la anchura (como mínimo 4): 7
+* * * * * * *
+*           *
+*       &   *
+* * * * * * *
+ */
 public class Ej10 {
     public static void main(String[] args) {
         Scanner ro=new Scanner(System.in);
